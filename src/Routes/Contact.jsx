b85b04a1a1bc61a816e9ac6a1c651c86644dfb87
Sachin Ruhela <156918990+SachinRuhela1922@users.noreply.github.com>
2026-07-03 17:42:06 +0530

    Update Contact.jsx

--- src/Routes/Contact.jsx
+++ src/Routes/Contact.jsx
@@ -175,7 +175,7 @@ function Contact() {
                 <h3>Find Us Here</h3>
                 <div className="contact-map-placeholder">
                   <iframe
-                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d241317.11609823204!2d72.7411015429981!3d19.08219783958249!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7c6306644edc1%3A0x5da4ed8f8d648c69!2sMumbai%2C%20Maharashtra!5e0!3m2!1sen!2sin!4v1700000000000!5m2!1sen!2sin"
+                    src="https://maps.app.goo.gl/28oESa6S7EaMKJez6"
                     width="100%"
                     height="100%"
                     style={{ border: 0 }}
@@ -188,7 +188,7 @@ function Contact() {
                 <div className="contact-map-details">
                   <div>
                     <strong>📌 Head Office</strong>
-                    <p>Berc, Sarvodhya Colony, Meerut Road, Uttar Pradesh - 245304</p>
+                    <p>Berc, Sarvodhya Colony, Meerut Road, Uttar Pradesh - 245101</p>
                   </div>
                   <div>
                     <strong>🕐 Working Hours</strong>
@@ -366,7 +366,7 @@ function Contact() {
                 <span>📱</span>
                 <div>
                   <strong>WhatsApp</strong>
-                  <span>+91 98765 43210</span>
+                  <span>+91 9760401780</span>
                 </div>
               </a>
               <a href="#" className="contact-social-link">
